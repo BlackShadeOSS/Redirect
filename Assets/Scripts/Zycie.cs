e620a5f3d4eb70bb7917866fdf54d6200e48c1fd
DarekParodia <darekparodia@gmail.com>
2025-03-15 02:10:22 +0100

    zycie

---
+++ Assets/Scripts/Zycie.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Zycie : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private const float maxHealth = 5f;
+    [SerializeField] private float health = maxHealth;
+    [SerializeField] private bool _isDead = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void resetHealth()
+    {
+        health = maxHealth;
+        _isDead = false;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        checkHealth();
+    }
+
+    public bool isDead()
+    {
+        return _isDead;
+    }
+
+    private void checkHealth()
+    {
+        if (health <= 0)
+        {
+            this._isDead = true;
+        }
+    }
+    
+}
